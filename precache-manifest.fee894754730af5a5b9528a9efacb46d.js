2916eec9a4d5241ea24c5a3f37fffab9d8b211ce
ashishkmsingh98 <ashishkmsingh98@gmail.com>
2019-10-15 10:50:23 +0530

    Updates

---
+++ precache-manifest.fee894754730af5a5b9528a9efacb46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b3fb2b614858fcb7851b15d53956d0",
+    "url": "/apps/index.html"
+  },
+  {
+    "revision": "253ccff375f1d3caf9ea",
+    "url": "/apps/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f070f027a041f388766b",
+    "url": "/apps/static/css/main.7488e9d3.chunk.css"
+  },
+  {
+    "revision": "253ccff375f1d3caf9ea",
+    "url": "/apps/static/js/2.991dd4ce.chunk.js"
+  },
+  {
+    "revision": "f070f027a041f388766b",
+    "url": "/apps/static/js/main.c7933bd0.chunk.js"
+  },
+  {
+    "revision": "f371fad1c8a09c1d498b",
+    "url": "/apps/static/js/runtime-main.91ad543e.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/apps/static/media/bg.b6796c73.jpg"
+  }
+]);
